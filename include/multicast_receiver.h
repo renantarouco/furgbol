21130c6de99780121426a7841f00ab6904859806
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2019-05-04 14:04:37 -0300

    implementação básica para eexemplificar uso do style guide do google

---
+++ include/multicast_receiver.h
@@ -0,0 +1,16 @@
+#ifndef FURGBOL_IO_MULTICAST_RECEIVER_H
+#define FURGBOL_IO_MULTICAST_RECEIVER_H
+
+namespace fbol {
+namespace io {
+
+  class MulticastReceiver {
+  public:
+    MulticastReceiver();
+    ~MulticastReceiver();
+  };
+
+} //namespace io
+} // namespace fbol
+
+#endif // FURGBOL_IO_MULTICAST_RECEIVER_H
