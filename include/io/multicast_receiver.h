2c2e6539b2f4ee567925d134fd6ea990f54be980
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2019-05-07 15:00:17 -0300

    adicionados submodulos RxCpp e cpplint e o arquivo lint.sh

---
+++ include/io/multicast_receiver.h
@@ -0,0 +1,18 @@
+// Copyright 2019 FURGBot
+
+#ifndef IO_MULTICAST_RECEIVER_H
+#define IO_MULTICAST_RECEIVER_H
+
+namespace furgbol {
+namespace io {
+
+class MulticastReceiver {
+ public:
+  MulticastReceiver();
+  ~MulticastReceiver();
+};
+
+}  // namespace io
+}  // namespace furgbol
+
+#endif  // IO_MULTICAST_RECEIVER_H
---
+++ src/io/multicast_receiver.cc
@@ -0,0 +1,12 @@
+// Copyright 2019 FURGBot
+
+#include "io/multicast_receiver.h"
+
+namespace furgbol {
+namespace io {
+
+MulticastReceiver::MulticastReceiver() {}
+MulticastReceiver::~MulticastReceiver() {}
+
+}
+}
