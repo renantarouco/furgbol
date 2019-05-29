55680d634f42adcea41fdd74a7c53740195d5a59
Thiago Jansen Sampaio Costa <thhiaggo1@hotmail.com>
2019-05-29 13:23:50 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,6 @@
 #include <iostream>
 
 int main() {
-    int cont = 0;
-    while(1){
-        furgbol::io::MulticastReceiver mcr("224.5.23.2",10020);
-        std::cout<<"b "<<mcr.get_s()<<std::endl;
-        std::cout<<"Tempo: "<<(cont++)<<" Tamanho da mensagem: "<<mcr.get_size()<<std::endl;
-    }
+    furgbol::io::MulticastReceiver mcr("224.5.23.2",10020);
+    std::cout<<" Tamanho da mensagem: "<<mcr.get_size()<<std::endl;
 }
